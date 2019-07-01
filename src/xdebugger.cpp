a52ccca0ad07d5aaf97ebac87e55bbc8bfde04b5
Johan Mabille <johan.mabille@gmail.com>
2019-07-01 09:20:30 +0200

    Added first test of debugger

--- src/xdebugger.cpp
+++ src/xdebugger.cpp
@@ -30,25 +30,38 @@ using namespace pybind11::literals;
 namespace xpyt
 {
     debugger::debugger(zmq::context_t& context,
-                       const xeus::xconfiguration&)
-        : m_ptvsd_client(context, xeus::get_socket_linger())
-        , m_ptvsd_socket(context, zmq::socket_type::dealer)
+                       const xeus::xconfiguration& config,
+                       const std::string& user_name,
+                       const std::string& session_id)
+        : m_ptvsd_client(context, config, xeus::get_socket_linger(), user_name, session_id)
+        , m_ptvsd_socket(context, zmq::socket_type::req)
+        , m_ptvsd_header(context, zmq::socket_type::req)
         , m_is_started(false)
     {
         m_ptvsd_socket.setsockopt(ZMQ_LINGER, xeus::get_socket_linger());
+        m_ptvsd_header.setsockopt(ZMQ_LINGER, xeus::get_socket_linger());
     }
 
-    nl::json debugger::process_request_impl(const nl::json& message)
+    nl::json debugger::process_request_impl(const nl::json& header,
+                                            const nl::json& message)
     {
-        nl::json reply;
+        nl::json reply = nl::json::object();
 
-        if(message["command"] == "attach")
+        if(message["command"] == "initialize" || message["command"] == "attach")
         {
+            std::cout << "XEUS-PYTHON: starting debugger" << std::endl;
             start();
+            std::cout << "XEUS-PYTHON: debugger has started" << std::endl;
         }
 
         if(m_is_started)
         {
+            std::string header_buffer = header.dump();
+            zmq::message_t raw_header(header_buffer.c_str(), header_buffer.length());
+            m_ptvsd_header.send(raw_header);
+            // client responds with ACK message
+            m_ptvsd_header.recv(&raw_header);
+
             std::string content = message.dump();
             size_t content_length = content.length();
             std::string buffer = xptvsd_client::HEADER
@@ -59,14 +72,16 @@ namespace xpyt
             m_ptvsd_socket.send(raw_message);
 
             zmq::message_t raw_reply;
-            m_ptvsd_socket.recv(&raw_message);
+            m_ptvsd_socket.recv(&raw_reply);
 
             reply = nl::json::parse(raw_reply.data<const char>());
         }
 
         if(message["command"] == "disconnect")
         {
+            std::cout << "XEUS-PYTHON: stopping debugger" << std::endl;
             stop();
+            std::cout << "XEUS-PYTHON: debugger has stopped" << std::endl;
         }
 
         return reply;
@@ -87,18 +102,24 @@ namespace xpyt
         }
 
         std::string controller_end_point = xeus::get_controller_end_point("debugger");
+        std::string controller_header_end_point = xeus::get_controller_end_point("debugger_header");
         std::string publisher_end_point = xeus::get_publisher_end_point();
 
         m_ptvsd_socket.bind(controller_end_point);
+        m_ptvsd_header.bind(controller_header_end_point);
 
         std::thread client(&xptvsd_client::start_debugger,
                            &m_ptvsd_client,
                            ptvsd_end_point,
                            publisher_end_point,
-                           controller_end_point);
+                           controller_end_point,
+                           controller_header_end_point);
         client.detach();
+        
+        m_ptvsd_socket.send(zmq::message_t("REQ", 3));
         zmq::message_t ack;
         m_ptvsd_socket.recv(&ack);
+
         m_is_started = true;
     }
 
@@ -110,9 +131,11 @@ namespace xpyt
     }
     
     std::unique_ptr<xeus::xdebugger> make_python_debugger(zmq::context_t& context,
-                                                          const xeus::xconfiguration& config)
+                                                          const xeus::xconfiguration& config,
+                                                          const std::string& user_name,
+                                                          const std::string& session_id)
     {
-        return std::unique_ptr<xeus::xdebugger>(new debugger(context, config));
+        return std::unique_ptr<xeus::xdebugger>(new debugger(context, config, user_name, session_id));
     }
 }
 
---
+++ test/xeus_client.hpp
@@ -0,0 +1,123 @@
+/***************************************************************************
+* Copyright (c) 2018, Martin Renou, Johan Mabille, Sylvain Corlay and      *
+* Wolf Vollprecht                                                          *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include <mutex>
+#include <queue>
+#include <string>
+
+#include "zmq.hpp"
+#include "nlohmann/json.hpp"
+#include "xeus/xauthentication.hpp"
+#include "xeus/xkernel_configuration.hpp"
+
+// Base class for clients, provides an API to 
+// send and receive messages, but nothing more ;)
+
+namespace nl = nlohmann;
+
+class xeus_client_base
+{
+public:
+
+    xeus_client_base(zmq::context_t& context,
+                     const std::string& user_name,
+                     const xeus::xconfiguration& config);
+
+    virtual ~xeus_client_base();
+
+    void subscribe_iopub(const std::string& filter);
+    void unsubscribe_iopub(const std::string& filter);
+
+protected:
+
+    void send_on_shell(nl::json header,
+                       nl::json parent_header,
+                       nl::json metadata,
+                       nl::json content);
+    nl::json receive_on_shell();
+
+    void send_on_control(nl::json header,
+                         nl::json parent_header,
+                         nl::json metadata,
+                         nl::json content);
+    nl::json receive_on_control();
+
+    nl::json receive_on_iopub();
+
+    nl::json make_header(const std::string& msg_type) const;
+    nl::json aggregate(const nl::json& header,
+                       const nl::json& parent_header,
+                       const nl::json& metadata,
+                       const nl::json& content) const;
+
+private:
+
+    void send_message(nl::json header,
+                      nl::json parent_header,
+                      nl::json metadata,
+                      nl::json content,
+                      zmq::socket_t& socket,
+                      const xeus::xauthentication& auth);
+
+    nl::json receive_message(zmq::socket_t& socket,
+                             const xeus::xauthentication& auth);
+
+    using authentication_ptr = std::unique_ptr<xeus::xauthentication>;
+    authentication_ptr p_shell_authentication;
+    authentication_ptr p_control_authentication;
+    authentication_ptr p_iopub_authentication;
+
+    zmq::socket_t m_shell;
+    zmq::socket_t m_control;
+    zmq::socket_t m_iopub;
+
+    std::string m_shell_end_point;
+    std::string m_control_end_point;
+    std::string m_iopub_end_point;
+
+    std::string m_user_name;
+    std::string m_session_id;
+};
+
+// Client that logs sent and received messages.
+// Runs the iopub poller in a dedicated thread and
+// push messages in a queue for future usage.
+class xeus_logger_client : public xeus_client_base
+{
+public:
+
+    xeus_logger_client(zmq::context_t& context,
+                       const std::string& user_name,
+                       const xeus::xconfiguration& config,
+                       const std::string& file_name);
+
+    virtual ~xeus_logger_client() = default;
+
+    void send_on_shell(const std::string& msg_type, nl::json content);
+    void send_on_control(const std::string& msg_type, nl::json content);
+
+    nl::json receive_on_shell();
+    nl::json receive_on_control();
+
+    std::size_t iopub_queue_size() const;
+    nl::json pop_iopub_message();
+
+private:
+
+    using base_type = xeus_client_base;
+
+    void poll_iopub();
+    void log_message(nl::json msg);
+
+    std::string m_file_name;
+    std::queue<nl::json> m_message_queue;
+    std::mutex m_file_mutex;
+    mutable std::mutex m_queue_mutex;
+};
+
