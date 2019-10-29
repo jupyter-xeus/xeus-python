befdfe8218ae398381b52450a4f82c36ad084829
martinRenou <martin.renou@gmail.com>
2019-10-29 14:40:40 +0100

    Update Jedi + minor inspection improvements
    - keyword argument default values in function signatures
    - "Name" -> "Signature" in the case of a function
    - colorize the inspection result
    - remove the signature from the docstring

--- src/xinspect.cpp
+++ src/xinspect.cpp
@@ -68,40 +68,53 @@ namespace xpyt
         auto docstring = definition.attr("docstring")().cast<std::string>();
         auto type = definition.attr("type").cast<std::string>();
 
-        std::vector<std::string> params;
-        // This code will throw if there is no params
-        try
+        std::string result;
+
+        // Retrieving the argument names and default values for keyword arguments if it's a function
+        py::list signatures = definition.attr("get_signatures")();
+        if (py::len(signatures) != 0)
         {
-            py::list py_params = definition.attr("params");
+            py::list py_params = signatures[0].attr("params");
+            result.append(red_text("Signature: ") + name + blue_text("("));
+
             for (py::handle param : py_params)
             {
-                // keyword arguments?? default value??
-                params.push_back(param.attr("name").cast<std::string>());
-            }
-        }
-        catch (...)
-        {
-        }
+                py::list param_description = param.attr("to_string")().attr("split")("=");
+                std::string param_name = param_description[0].cast<std::string>();
 
-        std::string result;
-        if (type.compare("function") == 0)
-        {
-            result.append(red_text("Name:  ") + name + "(");
-            for (auto it = params.cbegin(); it < params.cend() - 1; it++)
-            {
-                result.append(*it + ", ");
+                // The argument is not a kwarg (no default value)
+                if (py::len(param_description) == 1)
+                {
+                    result.append(param_name);
+                }
+                else
+                {
+                    std::string default_value = param_description[1].cast<std::string>();
+                    result.append(param_name + blue_text("=") + green_text(default_value));
+                }
+
+                // If it's not the last element, add a comma.
+                if (!param.is(py_params[py::len(py_params) - 1]))
+                {
+                    result.append(blue_text(", "));
+                }
             }
-            result.append(params.at(params.size() - 1) + ")");
+
+            result.append(blue_text(")"));
+
+            // Remove signature from the docstring
+            py::list splitted_docstring = definition.attr("docstring")().attr("split")("\n\n", 1);
+            if (py::len(splitted_docstring) == 2)
+            {
+                docstring = splitted_docstring[1].cast<std::string>();
+            }
         }
         else
         {
-            result.append(red_text("Name:  ") + name);
+            result.append(red_text("Name: ") + name);
         }
 
-        result.append(
-            red_text("  Type:  ") + type + "\n" +
-            red_text("Docstring:\n") + docstring
-        );
+        result.append(red_text("\nType: ") + type + red_text("\nDocstring: ") + docstring);
 
         return result;
     }
