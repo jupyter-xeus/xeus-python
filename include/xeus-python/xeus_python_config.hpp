cfb236bf917c55c5b8180d2e8fff5ef697fcf091
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-01-15 19:44:53 +0100

    Release 0.6.9

--- include/xeus-python/xeus_python_config.hpp
+++ include/xeus-python/xeus_python_config.hpp
@@ -14,7 +14,7 @@
 // Project version
 #define XPYT_VERSION_MAJOR 0
 #define XPYT_VERSION_MINOR 6
-#define XPYT_VERSION_PATCH 8
+#define XPYT_VERSION_PATCH 9
 
 // Composing the version string from major, minor and patch
 #define XPYT_CONCATENATE(A, B) XPYT_CONCATENATE_IMPL(A, B)
