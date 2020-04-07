0ac81ec756dfefc79241e2d21509eb17ff1217e5
martinRenou <martin.renou@gmail.com>
2020-04-07 10:39:58 +0200

    Implement Javascript class
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- src/xdisplay.cpp
+++ src/xdisplay.cpp
@@ -322,7 +322,7 @@ namespace xpyt
     {
     public:
 
-        xdisplay_object(const py::object& data, const py::object& url, const py::object& filename, const py::object& metadata, const std::string& read_flag = "r");
+        xdisplay_object(const py::object& data, const py::object& url, const py::object& filename, const py::object& metadata = py::dict(), const std::string& read_flag = "r");
         virtual ~xdisplay_object();
 
         xdisplay_object (const xdisplay_object&) = delete;
@@ -486,7 +486,7 @@ namespace xpyt
     {
     public:
 
-        xtext_display_object(const py::object& data, const py::object& url, const py::object& filename, const py::object& metadata);
+        xtext_display_object(const py::object& data, const py::object& url, const py::object& filename, const py::object& metadata = py::dict());
         virtual ~xtext_display_object();
 
     };
@@ -533,6 +533,87 @@ namespace xpyt
     py::object xhtml::html() const
     {
         return repr_html();
+    }
+
+    /*********************
+     * xjavascript class *
+     *********************/
+
+    class xjavascript : public xtext_display_object
+    {
+    public:
+
+        xjavascript(const py::object& data, const py::object& url, const py::object& filename, const py::object& lib, const py::object& css);
+        virtual ~xjavascript();
+
+        py::object repr_javascript();
+
+    private:
+
+        py::list m_lib;
+        py::list m_css;
+    };
+
+    xjavascript::xjavascript(const py::object& data, const py::object& url, const py::object& filename, const py::object& lib, const py::object& css)
+        : xtext_display_object(data, url, filename)
+    {
+        if (py::isinstance<py::str>(lib))
+        {
+            m_lib = py::list({lib});
+        }
+        else if (lib.is_none())
+        {
+            m_lib = py::list();
+        }
+        else
+        {
+            m_lib = lib;
+        }
+
+        if (py::isinstance<py::str>(css))
+        {
+            m_css = py::list({css});
+        }
+        else if (css.is_none())
+        {
+            m_css = py::list();
+        }
+        else
+        {
+            m_css = css;
+        }
+    }
+
+    xjavascript::~xjavascript()
+    {
+    }
+
+    py::object xjavascript::repr_javascript()
+    {
+        std::ostringstream string_stream;
+
+        for (py::handle css : m_css)
+        {
+            string_stream << R"($("head").append($("<link/>").attr({rel:  "stylesheet", type: "text/css", href: ")";
+            string_stream << css.cast<std::string>();
+            string_stream << R"("})))";
+        }
+
+        for (py::handle lib : m_lib)
+        {
+            string_stream << R"($.getScript(")";
+            string_stream << lib.cast<std::string>();
+            string_stream << R"(", function () {)";
+        }
+
+        string_stream << get_data().cast<std::string>();
+
+        for (std::size_t i = 0; i < py::len(m_lib); i++)
+        {
+            string_stream << "});";
+        }
+
+        return py::str(string_stream.str());
     }
 
     /*******************
@@ -1050,6 +1131,12 @@ namespace xpyt
                 py::arg("data") = py::none(), py::arg("url") = py::none(), py::arg("filename") = py::none(), py::arg("metadata") = py::none())
             .def("_repr_html_", &xhtml::repr_html)
             .def("__html__", &xhtml::html);
+
+        py::class_<xjavascript, xtext_display_object>(display_module, "Javascript")
+            .def(
+                py::init<const py::object&, const py::object&, const py::object&, const py::object&, const py::object&>(),
+                py::arg("data") = py::none(), py::arg("url") = py::none(), py::arg("filename") = py::none(), py::arg("lib") = py::none(), py::arg("css") = py::none())
+            .def("_repr_javascript_", &xjavascript::repr_javascript);
 
         py::class_<xmarkdown, xdisplay_object>(display_module, "Markdown")
             .def(
