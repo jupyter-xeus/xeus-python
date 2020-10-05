bccb73190768bb435c30d406491458ccc9de15a6
Johan Mabille <johan.mabille@gmail.com>
2020-10-05 14:55:50 +0200

    Release 0.8.5

--- include/xeus-python/xeus_python_config.hpp
+++ include/xeus-python/xeus_python_config.hpp
@@ -14,7 +14,7 @@
 // Project version
 #define XPYT_VERSION_MAJOR 0
 #define XPYT_VERSION_MINOR 8
-#define XPYT_VERSION_PATCH 4
+#define XPYT_VERSION_PATCH 5
 
 // Composing the version string from major, minor and patch
 #define XPYT_CONCATENATE(A, B) XPYT_CONCATENATE_IMPL(A, B)
