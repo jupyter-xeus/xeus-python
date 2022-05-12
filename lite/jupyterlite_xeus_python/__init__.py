608258fab060de80405371ec999261c16b9f0d91
martinRenou <martin.renou@gmail.com>
2022-05-12 08:53:11 +0200

    Release 0.1.0

--- lite/jupyterlite_xeus_python/__init__.py
+++ lite/jupyterlite_xeus_python/__init__.py
@@ -9,6 +9,7 @@ HERE = Path(__file__).parent.resolve()
 with (HERE / "labextension" / "package.json").open() as fid:
     data = json.load(fid)
 
+
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
--- lite/jupyterlite_xeus_python/_version.py
+++ lite/jupyterlite_xeus_python/_version.py
@@ -6,7 +6,7 @@ __all__ = ["__version__"]
 def _fetchVersion():
     HERE = Path(__file__).parent.resolve()
 
-    for settings in HERE.rglob("package.json"): 
+    for settings in HERE.rglob("package.json"):
         try:
             with settings.open() as f:
                 version = json.load(f)["version"]
