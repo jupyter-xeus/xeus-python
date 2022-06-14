7c4a29d3947ee25e342ba285187083aa192fb3ee
martinRenou <martin.renou@gmail.com>
2022-06-14 12:26:22 +0200

    Make xeus-python version configurable

--- lite/jupyterlite_xeus_python/__init__.py
+++ lite/jupyterlite_xeus_python/__init__.py
@@ -10,10 +10,24 @@ HERE = Path(__file__).parent.resolve()
 def _jupyter_labextension_paths():
     prefix = sys.prefix
     # For when in dev mode
-    if (HERE.parent / 'share' / 'jupyter' / 'labextensions' / '@jupyterlite' / 'xeus-python-kernel').parent.exists():
+    if (
+        HERE.parent
+        / "share"
+        / "jupyter"
+        / "labextensions"
+        / "@jupyterlite"
+        / "xeus-python-kernel"
+    ).parent.exists():
         prefix = HERE.parent
 
-    return [{
-        'src': prefix / 'share' / 'jupyter' / 'labextensions' / '@jupyterlite' / 'xeus-python-kernel',
-        'dest': '@jupyterlite/xeus-python-kernel',
-    }]
+    return [
+        {
+            "src": prefix
+            / "share"
+            / "jupyter"
+            / "labextensions"
+            / "@jupyterlite"
+            / "xeus-python-kernel",
+            "dest": "@jupyterlite/xeus-python-kernel",
+        }
+    ]
