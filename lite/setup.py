eae3405b3f70a945703216c269d1d4c4d7e07066
martinRenou <martin.renou@gmail.com>
2022-08-04 15:33:43 +0200

    Update to empack 1 with support for loading custom empack config

--- lite/setup.py
+++ lite/setup.py
@@ -57,7 +57,7 @@ setup_args = dict(
         "traitlets",
         "jupyterlite",
         "requests",
-        "empack>=0.8.2,<0.9",
+        "empack>=1.0.0,<2",
     ],
     zip_safe=False,
     include_package_data=True,
