2be3ba17715c8337343fc3ee875f684fe873ae1c
martinRenou <martin.renou@gmail.com>
2022-10-20 11:26:50 +0200

    Remove leftover

--- include/xeus-python/xinterpreter.hpp
+++ include/xeus-python/xinterpreter.hpp
@@ -79,7 +79,6 @@ namespace xpyt
         py::object m_displayhook;
         py::object m_logger;
         py::object m_terminal_stream;
-        py::object m_comm_manager;
 
         // The interpreter has the same scope as a `gil_scoped_release` instance
         // so that the GIL is not held by default, it will only be held when the
