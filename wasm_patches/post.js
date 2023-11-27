852514d1ea824d091fc940959e31aa57799103d9
DerThorsten <derthorstenbeier@gmail.com>
2023-11-27 10:09:36 +0100

    unify

--- wasm_patches/post.js
+++ wasm_patches/post.js
@@ -3,3 +3,17 @@ Module.UTF8ToString = UTF8ToString;
 Module.wasmTable = wasmTable;
 // Emscripten has a bug where it accidentally exposes an empty object as Module.ERRNO_CODES
 Module.ERRNO_CODES = ERRNO_CODES;
+
+
+
+Module['async_init'] = async function(
+    kernel_root_url, 
+    pkg_root_url,
+    verbose) {
+    Module['bootstrap_from_empack_packed_environment']
+    return Module['bootstrap_from_empack_packed_environment'](
+    `${kernel_root_url}/empack_env_meta.json`, /* packages_json_url */
+    pkg_root_url,               /* package_tarballs_root_url */
+    verbose                 /* verbose */
+    );
+}
