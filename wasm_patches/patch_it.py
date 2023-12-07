c1f85825d4fbe2bda048c5a43973e80c292ac9b4
DerThorsten <derthorstenbeier@gmail.com>
2023-12-07 10:53:51 +0100

    drop wasm patches

--- wasm_patches/patch_it.py
+++
@@ -1,9 +0,0 @@
-with open('./build/xpython_wasm.js', 'r') as f:
-    content = f.read()
-
-
-query = 'Module["preloadPlugins"].push(audioPlugin);'
-content = content.replace(query, '')
-
-with open('./build/xpython_wasm.js', 'w') as f:
-    f.write(content)
