952536cf9cfd1031d5ec60597ac374498fd757d7
Isabel Paredes <isabel.paredes@quantstack.net>
2024-04-19 15:56:24 +0200

    Fix internal request context

--- src/xinterpreter.cpp
+++ src/xinterpreter.cpp
@@ -338,8 +338,8 @@ namespace xpyt
 
             xerror error = extract_error(pyerror);
 
-            // TODO: where to get request context??
-            xeus::xrequest_context request_context(nl::json::object(), 1, 1);  // FIXME:
+            // For internal requests, the context is not required.
+            xeus::xrequest_context request_context(nl::json::object(), channel::SHELL, {});
             publish_execution_error(request_context, error.m_ename, error.m_evalue, error.m_traceback);
             error.m_traceback.resize(1);
             error.m_traceback[0] = code;
