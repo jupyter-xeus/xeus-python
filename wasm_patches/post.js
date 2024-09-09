ca1372b1879583d6288b3392d36ab2ea5ba9a344
Anastasiia Sliusar <anastasiia.sliusar@gmail.com>
2024-09-09 16:13:27 +0200

    Allow to setup custom link for empack_env_meta json file

--- wasm_patches/post.js
+++ wasm_patches/post.js
@@ -9,11 +9,27 @@ Module.ERRNO_CODES = ERRNO_CODES;
 Module['async_init'] = async function(
     kernel_root_url, 
     pkg_root_url,
-    verbose) {
+    verbose,
+    url_empack_env_meta = '') {
+
+    /* It should work with any kernel so we have to keep logic for this too
+       by default it will work with local empack_env_meta.json
+    */
+    let packages_json_url = `${kernel_root_url}/empack_env_meta.json`;
+    let isEmpackEnvMetaHosted = false;
+
+    // if we host empack_env_meta somewhere else then we have to proceed it
+    if (url_empack_env_meta) {
+     packages_json_url = url_empack_env_meta;
+     isEmpackEnvMetaHosted = true;
+    }
+     
+
     Module['bootstrap_from_empack_packed_environment']
     return Module['bootstrap_from_empack_packed_environment'](
-    `${kernel_root_url}/empack_env_meta.json`, /* packages_json_url */
+    packages_json_url,
     pkg_root_url,               /* package_tarballs_root_url */
-    verbose                     /* verbose */
+    verbose,                    /* verbose */
+    isEmpackEnvMetaHosted      /* if we host empack_env_meta somewhere else */             
     );
 }
