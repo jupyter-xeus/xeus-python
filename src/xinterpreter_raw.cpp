9bfdfe2e79dad9f657aefc0f682342659b99820e
arjxn-py <arjxn.py@gmail.com>
2025-10-27 15:54:03 +0530

    without callback

--- src/xinterpreter_raw.cpp
+++ src/xinterpreter_raw.cpp
@@ -227,7 +227,7 @@ namespace xpyt
 
         kernel_res = xeus::create_successful_reply(execution_count);
 
-        return cb(kernel_res);
+        return kernel_res;
     }
 
     nl::json raw_interpreter::complete_request_impl(
