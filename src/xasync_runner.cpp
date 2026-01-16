6059c301239dcc36e3aa38bbb5d6832888cbd46a
DerThorsten <derthorstenbeier@gmail.com>
2026-01-16 09:37:23 +0100

    use reader

--- src/xasync_runner.cpp
+++ src/xasync_runner.cpp
@@ -103,7 +103,7 @@ namespace xpyt
         int ZMQ_DONTWAIT{ 1 }; // from zmq.h 
         while (auto msg = read_shell(ZMQ_DONTWAIT))
         {
-            std::cout << "Shell message: " << msg.value() << std::endl;
+            std::cout << "got message on shell doorbell:\n"<<std::endl
             notify_shell_listener(std::move(msg.value()));
         }
     }
